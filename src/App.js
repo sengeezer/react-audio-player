fb8e439f67eef05f66c754bcbb56d660b290e364
Fabio Albertin <albertin@gmail.com>
2018-11-10 22:11:48 +0000

    Moved styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="app">
+        <header className="header">
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
+          <a 
+            className="link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
