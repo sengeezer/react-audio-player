b855647c0e084f99eeede37f7f723f576fd764da
Fabio Albertin <albertin@gmail.com>
2019-11-01 14:50:26 +0000

    Updates

---
+++ precache-manifest.5f289ea4dd90943a0f314a6196eff87b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "defd37dbdbca68babba3",
+    "url": "/react-audio-player/static/js/runtime~main.defd37db.js"
+  },
+  {
+    "revision": "a2cba8fb0bdad09f8998",
+    "url": "/react-audio-player/static/js/main.a2cba8fb.chunk.js"
+  },
+  {
+    "revision": "bc0144f9750e5dec61df",
+    "url": "/react-audio-player/static/js/1.bc0144f9.chunk.js"
+  },
+  {
+    "revision": "a2cba8fb0bdad09f8998",
+    "url": "/react-audio-player/static/css/main.ae99df2a.chunk.css"
+  },
+  {
+    "revision": "114a990bf6b599360d919e072eba8439",
+    "url": "/react-audio-player/index.html"
+  }
+];
